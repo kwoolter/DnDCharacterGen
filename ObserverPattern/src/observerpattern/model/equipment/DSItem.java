a8941789c8b80bf53b799fdcc8d0983210f6baa0
kwoolter <keith.woolterton@gmail.com>
2017-05-28 10:16:41 +0800

    initial commit

---
+++ ObserverPattern/src/observerpattern/model/equipment/DSItem.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package observerpattern.model.equipment;
+
+/**
+ *
+ * @author KeithW
+ */
+public class DSItem {
+    
+    private String sItemName, sItemCategory;
+    private Float fWeight, fValue;
+    
+}
